5b1e5dc34543fb1bfac81f1f982507f956fe9d8a
svnitad <gorfadvijay1234@gmail.com>
2022-02-17 20:18:44 +0530

    Basic Routing (Basic Restricted)

--- src/App.js
+++ src/App.js
@@ -14,15 +14,39 @@ import InfoComponent from './components/Info'
 import AdminScreens from './screens/Admin Screens'
 import Forgot from './components/Forgot Password/Forgot'
 import ChnagePassword from './components/Forgot Password/ChangePassword'
+import { useStoreState } from 'easy-peasy'
+import { useLocation } from 'react-router'
 
 function App() {
-  const auth = true
+  const RequireAuth = () => {
+    const userDetails = useStoreState(state => state.userDetails)
+    let location = useLocation()
+
+    if (!userDetails[0]) {
+      return <Navigate to="/auth/login" state={{ from: location }} replace />
+    }
+
+    return <></>
+  }
+
+  // fetch state
+  const userDetails = useStoreState(state => state.userDetails)
+  let loggedIn = userDetails[0]?.data
+
   return (
     <Router>
       <div className="App">
         <Header />
         <div className="non_header_content relative">
           <Routes>
+            <Route
+              path="/"
+              element={
+                <RequireAuth>
+                  <p>You're logged In</p>
+                </RequireAuth>
+              }
+            />
             <Route path="/auth" element={<AuthScreen />}>
               <Route
                 path="login"
@@ -37,11 +61,39 @@ function App() {
               <Route path="forgot" element={<Forgot />} />
               <Route path="reset" element={<ChnagePassword />} />
             </Route>
-            <Route path="/admin" element={<AdminScreens />}>
-              <Route path="user/list-view" element={<UserListView />} />
-              <Route path="user/approval-request" element={<UserApprovalScreen />} />
+            <Route
+              path="/admin"
+              element={
+                <RequireAuth>
+                  <AdminScreens />
+                </RequireAuth>
+              }
+            >
+              <Route
+                path="user/list-view"
+                element={
+                  <RequireAuth>
+                    <UserListView />
+                  </RequireAuth>
+                }
+              />
+              <Route
+                path="user/approval-request"
+                element={
+                  <RequireAuth>
+                    <UserApprovalScreen />
+                  </RequireAuth>
+                }
+              />
             </Route>
-            <Route path="/profile" element={<ProfileSettingScreen />} />
+            <Route
+              path="/profile"
+              element={
+                <RequireAuth>
+                  <ProfileSettingScreen />
+                </RequireAuth>
+              }
+            />
           </Routes>
         </div>
       </div>
