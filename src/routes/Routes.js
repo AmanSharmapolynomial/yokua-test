c9089738058502456ec4d4663918bf184452fe72
Vijay <vijay@polynomial.ai>
2022-02-22 11:25:49 +0530

    Router Fix

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -1,7 +1,6 @@
 import React, { Children } from 'react'
 import Header from '../components/Header'
 import AdminScreen from '../screens/Admin Screens/User List View'
-import AuthScreen from '../screens/Auth Screen'
 import ProfileSettingScreen from '../screens/Profile Setting'
 import { BrowserRouter as Router, Route, Routes, Redirect } from 'react-router-dom'
 import { Navigate } from 'react-router'
@@ -18,7 +17,7 @@ import { useLocation } from 'react-router'
 import HomeScreen from '../screens/Home Screen'
 import PrivateRoute from './PrivateRoute'
 import AuthLayout from '../layouts/AuthLayout'
-const Rout = () => {
+const Routing = () => {
   return (
     <Router>
       <Routes>
@@ -30,27 +29,25 @@ const Rout = () => {
             </PrivateRoute>
           }
         />
-        <Route path="/auth" element={<AuthScreen />}>
-          <Route
-            path="auth/login"
-            element={
-              <AuthScreen>
-                <SignIn />
-                <InfoComponent />
-              </AuthScreen>
-            }
-          />
-          <Route
-            path="auth/register"
-            element={
-              <AuthScreen>
-                <SignUp />
-              </AuthScreen>
-            }
-          />
-          <Route path="auth/forgot-password" element={<Forgot />} />
-          <Route path="auth/reset-password" element={<ChnagePassword />} />
-        </Route>
+        <Route
+          path="/auth/login"
+          element={
+            <AuthLayout>
+              <SignIn />
+              <InfoComponent />
+            </AuthLayout>
+          }
+        />
+        <Route
+          path="/auth/register"
+          element={
+            <AuthLayout>
+              <SignUp />
+            </AuthLayout>
+          }
+        />
+        <Route path="/auth/forgot-password" element={<Forgot />} />
+        <Route path="/auth/reset-password" element={<ChnagePassword />} />
         <Route
           path="/admin"
           element={
@@ -82,4 +79,4 @@ const Rout = () => {
   )
 }
 
-export default Routes
+export default Routing
