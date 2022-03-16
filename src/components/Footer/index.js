597f33ae9b6dcdfc2b09d7637ecccbf4cca17a8d
Anonymus <alkeshhelicore@gmail.com>
2022-03-16 17:49:54 +0530

    commit

---
+++ src/components/Footer/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import '../../components/Footer/style.css';
+
+const Footer = () =>{
+    return(
+        <>
+        <div className="footerBtn">
+        <button className="btn">Impressum</button>
+        <button className="btn">Privacy Policy</button>
+        </div>
+        </>
+    );
+}
+
+export default Footer; 
