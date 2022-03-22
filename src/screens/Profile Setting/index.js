dde44733273fe6faf409db2c584ebeaa1694c754
iAnujShrivastava <anuj.shrivastava@ipsator.com>
2022-03-22 19:15:39 +0530

    profile page UI fixed

--- src/screens/Profile Setting/index.js
+++ src/screens/Profile Setting/index.js
@@ -4,7 +4,12 @@ import PrimaryHeading from '../../components/Primary Headings/index'
 
 import moment from 'moment'
 import API from '../../utils/api'
-import { getToken, getUserRoles, removeToken, removeUserRole } from '../../utils/token'
+import {
+  getToken,
+  getUserRoles,
+  removeToken,
+  removeUserRole,
+} from '../../utils/token'
 import { toast } from 'react-toastify'
 import DeleteModal from '../../components/Modals/Delete Modal/DeleteModal'
 import validator from 'validator'
@@ -27,7 +32,10 @@ const ProfileSettingScreen = () => {
   const [isLoading, setIsLoading] = useState(false)
   const [disabledInputAddress, setDisabledInputAddress] = useState(true)
   const [disabledInputPassword, setDisabledInputPassword] = useState(true)
-  const [disabledInputPasswordRetype, setDisabledInputPasswordRetype] = useState(true)
+  const [
+    disabledInputPasswordRetype,
+    setDisabledInputPasswordRetype,
+  ] = useState(true)
 
   const [editMode1, setEditMode1] = useState(false)
   const [editMode2, setEditMode2] = useState(false)
@@ -51,14 +59,17 @@ const ProfileSettingScreen = () => {
     const backendData = await API.get('/auth/profile_settings/')
     setProfileData(backendData.data)
     _setProfilePicture(
-      backendData.data.basic_profile?.avatar ? backendData.data.basic_profile?.avatar : placeholder
+      backendData.data.basic_profile?.avatar
+        ? backendData.data.basic_profile?.avatar
+        : placeholder
     )
     setIsLoading(false)
   }, [reloadData])
 
   useEffect(() => {
     nameRef.current.value = profileData.basic_profile?.full_name || 'chael'
-    emailRef.current.value = profileData.basic_profile?.email || 'tech@yokogawa.com'
+    emailRef.current.value =
+      profileData.basic_profile?.email || 'tech@yokogawa.com'
     addressRef.current.value = profileData.basic_profile?.company_name
     const tempCheckedArr = []
     profileData.news_letter?.map((nl, index) => {
@@ -85,7 +96,10 @@ const ProfileSettingScreen = () => {
       email: [email],
     }
     // /admin/delete_user
-    const afterDeleteMsg = await API.post('/admin/delete_user', payloadDeleteAccount)
+    const afterDeleteMsg = await API.post(
+      '/admin/delete_user',
+      payloadDeleteAccount
+    )
     console.log(afterDeleteMsg)
     removeToken()
     removeUserRole()
@@ -124,7 +138,10 @@ const ProfileSettingScreen = () => {
         />
       )}
       <div className="profile-setting-container">
-        <PrimaryHeading title={'Profile settings'} backgroundImage={'yk-back-image-profile'} />
+        <PrimaryHeading
+          title={'Profile settings'}
+          backgroundImage={'yk-back-image-profile'}
+        />
         <div className="profile-setting">
           <div className="profile-setting__info">
             <div>
@@ -152,9 +169,9 @@ const ProfileSettingScreen = () => {
             <div className="profile-setting__info_name">
               <h4 className="name">{profileData.basic_profile?.full_name}</h4>
               <p className="details">
-                {`FCP user since ${moment(profileData.basic_profile?.date_joined).format(
-                  'MMM Do YYYY'
-                )}`}{' '}
+                {`FCP user since ${moment(
+                  profileData.basic_profile?.date_joined
+                ).format('MMM Do YYYY')}`}{' '}
               </p>
             </div>
           </div>
@@ -329,7 +346,9 @@ const ProfileSettingScreen = () => {
                 <i
                   className="fa-solid fa-pen-to-square edit"
                   style={{
-                    color: disabledInputPasswordRetype ? 'var(--bgColor2)' : 'grey',
+                    color: disabledInputPasswordRetype
+                      ? 'var(--bgColor2)'
+                      : 'grey',
                   }}
                   onClick={() => {
                     setDisabledInputPasswordRetype(!disabledInputPasswordRetype)
@@ -339,7 +358,9 @@ const ProfileSettingScreen = () => {
             </div>
           </div>
           <div className="profile-setting__basic-profile profile-setting__box">
-            <h1 className="profile-setting__heading">SALES NEWS BY ROTA YOKOGAWA</h1>
+            <h1 className="profile-setting__heading">
+              SALES NEWS BY ROTA YOKOGAWA
+            </h1>
             <div className="sales-news_background">
               {isLoading ? (
                 <span>Loading...</span>
@@ -395,7 +416,10 @@ const ProfileSettingScreen = () => {
                       full_name: name,
                     }
 
-                    const afterUpdateMsg = await API.post('/auth/profile_settings/', payloadName)
+                    const afterUpdateMsg = await API.post(
+                      '/auth/profile_settings/',
+                      payloadName
+                    )
                     toast.success(afterUpdateMsg.data.message)
                   }
                 } else {
@@ -410,13 +434,18 @@ const ProfileSettingScreen = () => {
                     const payloadEmail = {
                       email,
                     }
-                    const afterUpdateMsg = await API.post('/auth/profile_settings/', payloadEmail)
+                    const afterUpdateMsg = await API.post(
+                      '/auth/profile_settings/',
+                      payloadEmail
+                    )
                     console.log(afterUpdateMsg.data.message)
                     toast.success(afterUpdateMsg.data.message)
                   }
                 } else {
                   if (editMode2) {
-                    toast.error('Please enter email in proper format - abc@xyz.com')
+                    toast.error(
+                      'Please enter email in proper format - abc@xyz.com'
+                    )
                   }
                 }
                 if (address && address != '') {
@@ -475,7 +504,10 @@ const ProfileSettingScreen = () => {
                 const payloadNews = {
                   news_letter: tempNLArray,
                 }
-                const afterUpdateNewsMsg = await API.post('auth/profile_settings/', payloadNews)
+                const afterUpdateNewsMsg = await API.post(
+                  'auth/profile_settings/',
+                  payloadNews
+                )
                 toast.success(afterUpdateNewsMsg.data.message)
                 setReloadData(!reloadData)
               }}
@@ -499,7 +531,9 @@ const ProfileSettingScreen = () => {
                       <div className="training_text">
                         <span>{training.training_topic}</span>
                         <span>{training.date}</span>
-                        <span>Latest cancellation date {training.cancellation_date}</span>
+                        <span>
+                          Latest cancellation date {training.cancellation_date}
+                        </span>
                         <span>{training.address}</span>
                       </div>
 
@@ -517,17 +551,19 @@ const ProfileSettingScreen = () => {
                 <span>Loading...</span>
               ) : (
                 <div className="profile-setting__basic-profile-edit">
-                  {profileData.participated_trainings?.map((training, index) => (
-                    <div className="edit_training" key={index}>
-                      <i className="fa-solid fa-calendar-check" />
-                      <div className="training_text">
-                        <span>{training.training_name}</span>
-                        <span>{training.date}</span>
-                      </div>
+                  {profileData.participated_trainings?.map(
+                    (training, index) => (
+                      <div className="edit_training" key={index}>
+                        <i className="fa-solid fa-calendar-check" />
+                        <div className="training_text">
+                          <span>{training.training_name}</span>
+                          <span>{training.date}</span>
+                        </div>
 
-                      <i className="fa-solid fa-pen-to-square edit" />
-                    </div>
-                  ))}
+                        <i className="fa-solid fa-pen-to-square edit" />
+                      </div>
+                    )
+                  )}
                 </div>
               )}
             </div>
