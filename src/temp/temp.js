e3ab516e9ac36e2ce72e488933ae59a8ea0b6124
kartik-ramani <karthik@polynomial.ai>
2022-03-25 10:30:02 +0530

    add temp file

---
+++ src/temp/temp.js
@@ -0,0 +1,27 @@
+
+<>
+
+    <div class="container pb-4">
+        <div class="yk-product-card row">
+            <div class="col-md-6 col-lg-6 col-xl-6">
+                <div class="img-section-two row mx-0">
+                    <div class="img-main d-block">
+                        <div class="img-box"><i class="fa fa-picture-o" aria-hidden="true"></i></div>
+                        <div class="img-bottom-two">Coriolis - Rotamass</div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-6 col-lg-6 col-xl-6" style="display: grid; place-items: center;">
+                <i class="fa fa-pencil-square-o" aria-hidden="true"></i>
+                <div class="yk-product-line-card-txt" style="height: auto;">
+
+                    <p>A paragraph of text with an<br/><a>unassigned link.</a></p>
+                    <p>A second row of text with a <a>web link.</a></p>
+
+                </div>
+
+            </div>
+        </div>
+    </div>
+    </>
