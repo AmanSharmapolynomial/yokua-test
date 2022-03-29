a69405a6e9c3c370c0a9d7b433808e670ba574de
kartik-ramani <karthik@polynomial.ai>
2022-03-29 17:04:05 +0530

    dropdown changes

--- src/sign-up-dropdown/Register-drp-dwn.js
+++ src/sign-up-dropdown/Register-drp-dwn.js
@@ -1,105 +1,106 @@
-<div class="container" style="margin-bottom: 30%; margin-top: 4%;">
-        <div class="yk-sign-up-dropdn">
-            <div class="row">
+import '../Register-dro-dwn.css';
+<div className="container" style="margin-bottom: 30%; margin-top: 4%;">
+        <div className="yk-sign-up-dropdn">
+            <div className="row">
 
-                <div class="dropdown">
-                    <div class="btn-group">
-                        <button class="btn btn-secondary btn-main btn-sm" type="button">
+                <div classNameName="dropdown yk-drop">
+                    <div classNameName="btn-group">
+                        <button className="btn btn-main bt-m btn-sm" type="button">
                            Choose your Product line
                         </button>
-                        <button type="button" class="btn btn-sm btn-secondary btn-arrow dropdown-toggle dropdown-toggle-split" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            <span class="sr-only">Toggle Dropdown</span>
+                        <button type="button" className="btn btn-sm btn-secondary btn-arrow dropdown-toggle dropdown-toggle-split" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            <span className="sr-only">Toggle Dropdown</span>
                         </button>
-                        <ul class="dropdown-menu multi-level" role="menu" aria-labelledby="dropdownMenu">
+                        <ul className="dropdown-menu multi-level" role="menu" aria-labelledby="dropdownMenu">
                                 
-                            <li class="dropdown-submenu">
-                                <a class="dropdown-item" tabindex="-1" href="#">RAMC <i class="fa fa-chevron-right mt-1" aria-hidden="true"></i></a>
-                                <ul class="dropdown-menu">
-                                    <li class="dropdown-item"><a tabindex="-1" href="#">RAMC - all</a></li>
-                                    <li class="dropdown-submenu">
+                            <li className="dropdown-submenu">
+                                <a className="dropdown-item" tabindex="-1" href="#">RAMC <i className="fa fa-chevron-right mt-1" aria-hidden="true"></i></a>
+                                <ul className="dropdown-menu">
+                                    <li className="dropdown-item"><a tabindex="-1" href="#">RAMC - all</a></li>
+                                    <li className="dropdown-submenu">
                                  
-                                        <ul class="dropdown-menu">
-                                            <li class="dropdown-item"><a href="#">3rd level</a></li>
-                                            <li class="dropdown-submenu"><a class="dropdown-item" href="#">another level</a>
-                                                <ul class="dropdown-menu">
-                                                    <li class="dropdown-item"><a href="#">4th level</a></li>
-                                                    <li class="dropdown-item"><a href="#">4th level</a></li>
-                                                    <li class="dropdown-item"><a href="#">4th level</a></li>
+                                        <ul className="dropdown-menu">
+                                            <li className="dropdown-item"><a href="#">3rd level</a></li>
+                                            <li className="dropdown-submenu"><a className="dropdown-item" href="#">another level</a>
+                                                <ul className="dropdown-menu">
+                                                    <li className="dropdown-item"><a href="#">4th level</a></li>
+                                                    <li className="dropdown-item"><a href="#">4th level</a></li>
+                                                    <li className="dropdown-item"><a href="#">4th level</a></li>
                                                 </ul>
                                             </li>
-                                            <li class="dropdown-item"><a href="#">3rd level</a></li>
+                                            <li className="dropdown-item"><a href="#">3rd level</a></li>
                                         </ul>
                                     </li>
-                                    <li class="dropdown-item"><a href="#">RAMC01</a></li>
-                                    <li class="dropdown-item"><a href="#">RAMC02</a></li>
+                                    <li className="dropdown-item"><a href="#">RAMC01</a></li>
+                                    <li className="dropdown-item"><a href="#">RAMC02</a></li>
                                 </ul>
                             </li>
                             
-                            <li class="dropdown-submenu">
-                                <a class="dropdown-item" tabindex="-1" href="#">RAMC <i class="fa fa-chevron-right mt-1" aria-hidden="true"></i></a>
-                                <ul class="dropdown-menu">
-                                    <li class="dropdown-item"><a tabindex="-1" href="#">Second level</a></li>
-                                    <li class="dropdown-submenu">
-                                        <ul class="dropdown-menu">
-                                            <li class="dropdown-item"><a href="#">3rd level</a></li>
-                                            <li class="dropdown-submenu"><a class="dropdown-item" href="#">another level</a>
-                                                <ul class="dropdown-menu">
-                                                    <li class="dropdown-item"><a href="#">4th level</a></li>
-                                                    <li class="dropdown-item"><a href="#">4th level</a></li>
-                                                    <li class="dropdown-item"><a href="#">4th level</a></li>
+                            <li className="dropdown-submenu m">
+                                <a className="dropdown-item" tabindex="-1" href="#">RAMC <i className="fa fa-chevron-right mt-1" aria-hidden="true"></i></a>
+                                <ul className="dropdown-menu">
+                                    <li className="dropdown-item"><a tabindex="-1" href="#">Second level</a></li>
+                                    <li className="dropdown-submenu">
+                                        <ul className="dropdown-menu">
+                                            <li className="dropdown-item"><a href="#">3rd level</a></li>
+                                            <li className="dropdown-submenu"><a className="dropdown-item" href="#">another level</a>
+                                                <ul className="dropdown-menu">
+                                                    <li className="dropdown-item"><a href="#">4th level</a></li>
+                                                    <li className="dropdown-item"><a href="#">4th level</a></li>
+                                                    <li className="dropdown-item"><a href="#">4th level</a></li>
                                                 </ul>
                                             </li>
-                                            <li class="dropdown-item"><a href="#">3rd level</a></li>
+                                            <li className="dropdown-item"><a href="#">3rd level</a></li>
                                         </ul>
                                     </li>
-                                    <li class="dropdown-item"><a href="#">Second level</a></li>
-                                    <li class="dropdown-item"><a href="#">Second level</a></li>
+                                    <li className="dropdown-item"><a href="#">Second level</a></li>
+                                    <li className="dropdown-item"><a href="#">Second level</a></li>
                                 </ul>
                             </li>
                             
-                            <li class="dropdown-submenu">
-                                <a class="dropdown-item" tabindex="-1" href="#">RAMC <i class="fa fa-chevron-right mt-1" aria-hidden="true"></i></a>
-                                <ul class="dropdown-menu">
-                                    <li class="dropdown-item"><a tabindex="-1" href="#">Second level</a></li>
-                                    <li class="dropdown-submenu">
+                            <li className="dropdown-submenu">
+                                <a className="dropdown-item" tabindex="-1" href="#">RAMC <i className="fa fa-chevron-right mt-1" aria-hidden="true"></i></a>
+                                <ul className="dropdown-menu">
+                                    <li className="dropdown-item"><a tabindex="-1" href="#">Second level</a></li>
+                                    <li className="dropdown-submenu">
                                 
-                                        <ul class="dropdown-menu">
-                                            <li class="dropdown-item"><a href="#">3rd level</a></li>
-                                            <li class="dropdown-submenu"><a class="dropdown-item" href="#">another level</a>
-                                                <ul class="dropdown-menu">
-                                                    <li class="dropdown-item"><a href="#">4th level</a></li>
-                                                    <li class="dropdown-item"><a href="#">4th level</a></li>
-                                                    <li class="dropdown-item"><a href="#">4th level</a></li>
+                                        <ul className="dropdown-menu">
+                                            <li className="dropdown-item"><a href="#">3rd level</a></li>
+                                            <li className="dropdown-submenu"><a className="dropdown-item" href="#">another level</a>
+                                                <ul className="dropdown-menu">
+                                                    <li className="dropdown-item"><a href="#">4th level</a></li>
+                                                    <li className="dropdown-item"><a href="#">4th level</a></li>
+                                                    <li className="dropdown-item"><a href="#">4th level</a></li>
                                                 </ul>
                                             </li>
-                                            <li class="dropdown-item"><a href="#">3rd level</a></li>
+                                            <li className="dropdown-item"><a href="#">3rd level</a></li>
                                         </ul>
                                     </li>
-                                    <li class="dropdown-item"><a href="#">Second level</a></li>
-                                    <li class="dropdown-item"><a href="#">Second level</a></li>
+                                    <li className="dropdown-item"><a href="#">Second level</a></li>
+                                    <li className="dropdown-item"><a href="#">Second level</a></li>
                                 </ul>
                             </li>
                             
-                            <li class="dropdown-submenu">
-                                <a class="dropdown-item" tabindex="-1" href="#">RAMC<i class="fa fa-chevron-right mt-1" aria-hidden="true"></i></a>
-                                <ul class="dropdown-menu">
-                                    <li class="dropdown-item"><a tabindex="-1" href="#">Second level</a></li>
-                                    <li class="dropdown-submenu">
+                            <li className="dropdown-submenu">
+                                <a className="dropdown-item" tabindex="-1" href="#">RAMC<i className="fa fa-chevron-right mt-1" aria-hidden="true"></i></a>
+                                <ul className="dropdown-menu">
+                                    <li className="dropdown-item"><a tabindex="-1" href="#">Second level</a></li>
+                                    <li className="dropdown-submenu">
                        
-                                        <ul class="dropdown-menu">
-                                            <li class="dropdown-item"><a href="#">3rd level</a></li>
-                                            <li class="dropdown-submenu"><a class="dropdown-item" href="#">another level</a>
-                                                <ul class="dropdown-menu">
-                                                    <li class="dropdown-item"><a href="#">4th level</a></li>
-                                                    <li class="dropdown-item"><a href="#">4th level</a></li>
-                                                    <li class="dropdown-item"><a href="#">4th level</a></li>
+                                        <ul className="dropdown-menu">
+                                            <li className="dropdown-item"><a href="#">3rd level</a></li>
+                                            <li className="dropdown-submenu"><a className="dropdown-item" href="#">another level</a>
+                                                <ul className="dropdown-menu">
+                                                    <li className="dropdown-item"><a href="#">4th level</a></li>
+                                                    <li className="dropdown-item"><a href="#">4th level</a></li>
+                                                    <li className="dropdown-item"><a href="#">4th level</a></li>
                                                 </ul>
                                             </li>
-                                            <li class="dropdown-item"><a href="#">3rd level</a></li>
+                                            <li className="dropdown-item"><a href="#">3rd level</a></li>
                                         </ul>
                                     </li>
-                                    <li class="dropdown-item"><a href="#">Second level</a></li>
-                                    <li class="dropdown-item"><a href="#">Second level</a></li>
+                                    <li className="dropdown-item"><a href="#">Second level</a></li>
+                                    <li className="dropdown-item"><a href="#">Second level</a></li>
                                 </ul>
                             </li>
                         </ul>
