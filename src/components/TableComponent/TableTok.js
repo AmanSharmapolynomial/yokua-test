1fe1d27cb0e2a6b056458dcd711915c643a70df3
kartik-ramani <karthik@polynomial.ai>
2022-04-06 10:26:02 +0530

    add Table in table components

---
+++ src/components/TableComponent/TableTok.js
@@ -0,0 +1,78 @@
+
+
+function UserGreeting( ) {
+    return (
+
+        <>
+        <div class="yk-table-t container">
+    
+    <table class="table">
+        <thead class="thead-dark">
+            <tr>
+                <th scope="col">Product model&nbsp;<i class="bi bi-caret-down-fill mt-2"></i></th>
+                <th scope="col">Process Connect&nbsp; <i class="bi bi-caret-down-fill"></i></th>
+                <th scope="col">Material &nbsp;<i class="bi bi-caret-down-fill"></i></th>
+                <th scope="col">Test/Certificate &nbsp;<i class="bi bi-caret-down-fill"></i></th>
+                <th scope="col">Other <i class="bi bi-caret-down-fill"></i></th>
+                <th scope="col">Y-Quote Number&nbsp; <i class="bi bi-caret-down-fill"></i></th>
+                <th scope="col">XJNo&nbsp; <i class="bi bi-caret-down-fill"></i></th>
+                <th scope="col">Valid until&nbsp; <i class="bi bi-caret-down-fill"></i></th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <th scope="row">1</th>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+                <td>@mdo</td>
+                <td>@mdo</td>
+                <td>@mdo</td>
+                <td>@mdo</td>
+            </tr>
+            
+            <tr>
+                <th scope="row">2</th>
+                <td>Jacob</td>
+                <td>Thornton</td>
+                <td>@fat</td>
+                <td>@mdo</td>
+                <td>@mdo</td>
+                <td>@mdo</td>
+                <td>@mdo</td>
+                
+            </tr>
+            
+            <tr>
+                <th scope="row">3</th>
+                <td>Larry</td>
+                <td>the Bird</td>
+                <td>@twitter</td>
+                <td>@mdo</td>
+                <td>@mdo</td>
+                <td>@mdo</td>
+                <td>@mdo</td>
+            </tr>
+            
+            <tr>
+                <th scope="row">4</th>
+                <td>Larry</td>
+                <td>the Bird</td>
+                <td>@twitter</td>
+                <td>@mdo</td>
+                <td>@mdo</td>
+                <td>@mdo</td>
+                <td>@mdo</td>
+            </tr>
+          
+        </tbody>
+    </table>
+    
+    </div>
+        
+        </>
+    )
+  }
+
+
+
