768f999ea3e62c052a6962f5fa83dc4b88178c6e
Ankur Khandelwal <khandelwalankur176@gmail.com>
2022-08-22 18:57:37 +0530

    feat: Product line table row edit feature

--- src/components/TableComponent/Table.js
+++ src/components/TableComponent/Table.js
@@ -9,6 +9,8 @@ import { toast } from 'react-toastify'
 import { getUserRoles } from '../../utils/token'
 import ic_link from '../../assets/link_icon.png'
 import Uploadicon from '../../assets/Icon awesome-file-download.png'
+import { Modal } from 'react-bootstrap'
+import { useLoading } from '../../utils/LoadingContext'
 
 /**
  *
@@ -52,6 +54,11 @@ export default ({
   const [isEditable, setIsEditable] = useState(false)
   const [sortMethod, setSortMethod] = useState('')
   const [editedTableObject, setEditedTableObject] = useState(tableObject)
+  const [editSelected, setEditSelected] = useState(null)
+  const [deleteSelected, setDeleteSelected] = useState(null)
+  const [archiveSelected, setArchiveSelected] = useState(null)
+  const [updatedProductFile, setUpdatedProductFile] = useState(null)
+  const { setLoading } = useLoading()
   const inputRef = useRef([])
   const customStyles = {
     table: {
@@ -101,8 +108,108 @@ export default ({
     },
   }
 
+  const inputEditStyle = {
+    borderBottom: '1px solid rgb(0, 79, 155)',
+    borderTop: 'none',
+    borderRight: 'none',
+    borderLeft: 'none',
+    maxWidth: 'max-content',
+  }
+
+  const updateTableValues = tableObject => {
+    setLoading(true)
+    let payload = {
+      action_type: 'update_cell',
+      update_objs: [],
+    }
+    for (let index = 0; index < tableObject.length; index++) {
+      if (tableObject[index].isEdited) {
+        const values = tableObject[index].values.filter(value => value.isEdited)
+        values.map(value => {
+          payload.update_objs.push({
+            table_id: value.table_id,
+            column_name: value.column_name,
+            row_index: value.row_index,
+            value: value.value,
+          })
+        })
+        //payload.update_objs.push(...values)
+      }
+    }
+    const formData = new FormData()
+    formData.append('data', JSON.stringify(payload))
+    if (updatedProductFile !== null) {
+      formData.append('file', updatedProductFile)
+    }
+    API.post('/products/page/update_table_data', formData)
+      .then(res => {
+        if (res.status === 200 && res.data !== undefined) {
+          if (res.data?.message) {
+            toast.success(res.data?.message)
+          }
+        }
+        setEditSelected(null)
+        setUpdatedProductFile(null)
+        onEditableClick()
+        onRefresh()
+        setLoading(false)
+      })
+      .catch(err => {
+        console.log(err)
+        setLoading(false)
+      })
+  }
+
+  const archiveRow = row => {
+    let payload = {
+      id: row.table_id,
+      archive_type: 'component',
+      component_type: 'table row',
+      row_index: row.row_index,
+    }
+
+    API.post('/products/page/set_archive', payload)
+      .then(data => {
+        toast.success('Row archived successfully')
+        setArchiveSelected(null)
+        onEditableClick()
+        onRefresh()
+        _setTableHeaders()
+        _setTableData()
+        onTableUpdate(editedTableObject)
+      })
+      .catch(err => {
+        toast.error('Error in Archiving')
+      })
+  }
+
+  const deleteRow = item => {
+    API.post('/products/page/delete_table_row', {
+      table_id: item.table_id,
+      row_index: item.row_index,
+    })
+      .then(data => {
+        toast.success('Row deleted successfully')
+        setDeleteSelected(null)
+        setEditSelected(null)
+        setIsEditable(false)
+        onEditableClick()
+        onRefresh()
+        _setTableHeaders()
+        _setTableData()
+        onTableUpdate(editedTableObject)
+      })
+      .catch(err => {})
+  }
+
   const _setTableHeaders = sort => {
     let tableColumns = []
+    if (isTableEditable && editSelected != null) {
+      tableColumns.push({
+        name: 'Edit File',
+        selector: row => row.editFile,
+      })
+    }
     tableObject?.map((column, index) => {
       const tempColumnName = column.column_name
       if (!column.is_filterable) {
@@ -168,7 +275,10 @@ export default ({
       }
     })
     tableColumns = tableColumns.filter(column => column.name != 'File')
-    // console.log("OOOOOOO", tableColumns)
+    tableColumns.push({
+      name: '',
+      selector: row => row.edit,
+    })
     setTableHeader([...tableColumns])
   }
   const handleSort = (column, type) => {
@@ -203,7 +313,7 @@ export default ({
     }
   }
 
-  const handleChange = (name, value, index) => {
+  const handleChange = (name, value, index, file = null) => {
     setEditedTableObject(prevState => {
       let arr = [...prevState]
       const columnIndex = arr.findIndex(ele => ele.column_name === name)
@@ -212,6 +322,7 @@ export default ({
           arr[columnIndex].isEdited = true
           arr[columnIndex].values[index].isEdited = true
           arr[columnIndex].values[index].value = value
+          if (file != null) setUpdatedProductFile(file)
         } catch (error) {
           console.log(error)
         }
@@ -228,30 +339,51 @@ export default ({
         const tableRowObject = {
           id: item.id,
         }
-        // console.log("JJJJJJ", tableData)
         const fileLinks = tableData.find(ele => ele.column_name == 'File')
-        console.log('FILE LINKS', fileLinks)
         tableData?.map((column_name, column_index) => {
-          if (!isTableEditable) {
-            const tempObject = new Object()
-            let value = column_name.values[index]?.value
-            let link = fileLinks?.values[index].value || '#'
-            if (column_name.column_name === 'Name') {
-              value = (
+          const tempObject = new Object()
+          const row_i = column_name.values[index]?.row_index
+          let value = column_name.values[index]?.value
+          let link = fileLinks?.values[index].value || '#'
+          if (column_name.column_name === 'Name') {
+            let name = value
+            value =
+              editSelected === row_i && isTableEditable ? (
+                <input
+                  style={inputEditStyle}
+                  type={item.isDate ? 'date' : 'text form-control'}
+                  value={name}
+                  onChange={e => handleChange(column_name['column_name'], e.target.value, index)}
+                />
+              ) : (
                 <a href={link} target="_blank">
                   {value}
                 </a>
               )
-            }
-            if (column_name.is_link) {
-              value = (
+          } else if (column_name.is_link) {
+            value =
+              editSelected === row_i && isTableEditable ? (
+                <input
+                  style={inputEditStyle}
+                  type={item.isDate ? 'date' : 'text form-control'}
+                  value={value}
+                  onChange={e => handleChange(column_name['column_name'], e.target.value, index)}
+                />
+              ) : (
                 <a href={value} target="_blank">
                   {value}
                 </a>
               )
-            }
-            if (column_name.is_file) {
-              value = (
+          } else if (column_name.is_file) {
+            value =
+              editSelected === row_i && isTableEditable ? (
+                <input
+                  style={inputEditStyle}
+                  type={item.isDate ? 'date' : 'text form-control'}
+                  value={value}
+                  onChange={e => handleChange(column_name['column_name'], e.target.value, index)}
+                />
+              ) : (
                 <a href={value} download>
                   <img
                     src={Uploadicon}
@@ -260,30 +392,86 @@ export default ({
                   />
                 </a>
               )
-            }
-            tempObject[column_name['column_name']] = value
-            Object.assign(tableRowObject, tempObject)
           } else {
-            const tempObject = new Object()
-            let value = column_name.values[index]?.value
-            tempObject[column_name['column_name']] = (
-              <>
+            value =
+              editSelected === row_i && isTableEditable ? (
                 <input
-                  style={{
-                    borderBottom: '1px solid rgb(0, 79, 155)',
-                    borderTop: 'none',
-                    borderRight: 'none',
-                    borderLeft: 'none',
-                  }}
+                  style={inputEditStyle}
                   type={item.isDate ? 'date' : 'text form-control'}
+                  disabled={
+                    column_name.column_name === 'Type' || column_name.column_name === 'Size'
+                  }
                   value={value}
                   onChange={e => handleChange(column_name['column_name'], e.target.value, index)}
                 />
-              </>
-            )
-            Object.assign(tableRowObject, tempObject)
+              ) : (
+                value
+              )
           }
+          tempObject[column_name['column_name']] = value
+          Object.assign(tableRowObject, tempObject)
         })
+        if (isTableEditable) {
+          tableRowObject.edit = (
+            <div>
+              {editSelected == item.row_index ? (
+                <i
+                  className="fa-solid fa-floppy-disk theme"
+                  style={{
+                    marginRight: '1rem',
+                  }}
+                  role={'button'}
+                  onClick={() => {
+                    updateTableValues(editedTableObject)
+                  }}
+                />
+              ) : (
+                <i
+                  className="fa-solid fa-pen-to-square ms-2 theme"
+                  style={{
+                    marginRight: '1rem',
+                  }}
+                  role={'button'}
+                  onClick={() => {
+                    setEditSelected(item.row_index)
+                  }}
+                />
+              )}
+
+              <i
+                className="fa-solid fa-box-archive theme"
+                style={{
+                  marginRight: '1rem',
+                }}
+                role={'button'}
+                onClick={() => {
+                  setArchiveSelected(item)
+                }}
+              />
+
+              <i
+                className="fa-solid fa-trash"
+                role={'button'}
+                onClick={() => {
+                  setDeleteSelected(item)
+                }}
+              />
+            </div>
+          )
+          if (editSelected == item.row_index) {
+            tableRowObject.editFile = (
+              <input
+                style={{
+                  all: 'initial',
+                }}
+                type={'file'}
+                onChange={e => {
+                  handleChange('File', '', index, e.target.files[0])
+                }}
+              />
+            )
+          }
+        }
         finalTableData.push(tableRowObject)
       })
     setTableRows([...finalTableData])
@@ -400,8 +588,9 @@ export default ({
     if (tableObject !== {}) {
       _setTableHeaders()
       _setTableData()
+      setEditedTableObject(tableObject)
     }
-  }, [tableObject, isTableEditable])
+  }, [tableObject, isTableEditable, editSelected])
 
   useEffect(() => {
     if (isTableEditable) {
@@ -416,84 +605,160 @@ export default ({
   }, [editedTableObject, isTableEditable])
 
   return (
-    <div className="col-12 mt-4">
-      <div className="row">
-        <div className="col p-0 table-header d-flex align-items-center">
-          <span style={{ fontSize: '1.25rem' }}>{table_name}</span>
-        </div>
-        {isAdmin && !archivedFilter && (
-          <div className="col-auto my-2 p-0 d-none d-lg-block">
-            <Image
-              className="me-2"
-              style={{ width: '1.4rem' }}
-              role={'button'}
-              src={ic_link}
-              onClick={() => {
-                onLinkClick()
-              }}
-            />
-            <i
-              role={'button'}
-              className={
-                !isTableEditable
-                  ? 'fa-solid fa-pen-to-square me-2 theme'
-                  : 'fa-solid fa-floppy-disk theme'
-              }
-              onClick={() => {
-                onEditableClick()
-                if (isTableEditable) {
-                  onTableUpdate(editedTableObject)
-                }
-              }}
-            />
-            <i
-              role={'button'}
-              className="fa-solid fa-trash ms-2 me-0"
-              onClick={() => {
-                onDeleteComponent()
-              }}
-            ></i>
+    <>
+      <div className="col-12 mt-4">
+        <div className="row">
+          <div className="col p-0 table-header d-flex align-items-center">
+            <span style={{ fontSize: '1.25rem' }}>{table_name}</span>
           </div>
-        )}
-      </div>
-      <div className="row">
-        <div className="border w-100 p-0 product-detail-table">
-          <DataTable
-            sortIcon={<i className="fa-solid fa-sort ms-1"></i>}
-            pagination={false}
-            paginationPerPage={false}
-            fixedHeader
-            columns={tableHeader}
-            data={tableRows}
-            customStyles={customStyles}
-            persistTableHead
-            sortFunction={customSort}
-            // conditionalRowStyles={conditionalRowStyles}
-            // selectableRows
-            // onSelectedRowsChange={selectedRowsActionUA}
-          />
-          {isEditable ? (
-            renderDummyRow()
-          ) : isAdmin && !isTableEditable && !archivedFilter ? (
-            <div
-              role={'button'}
-              className="add-row d-none d-lg-flex"
-              onClick={() => {
-                setIsEditable(true)
-              }}
-            >
-              <img
-                src={Plusicon}
-                style={{
-                  width: '16px',
-                  marginRight: '12px',
+          {isAdmin && !archivedFilter && (
+            <div className="col-auto my-2 p-0 d-none d-lg-block">
+              <Image
+                className="me-2"
+                style={{ width: '1.4rem' }}
+                role={'button'}
+                src={ic_link}
+                onClick={() => {
+                  onLinkClick()
                 }}
               />
-              Add
+              <i
+                role={'button'}
+                className={
+                  !isTableEditable
+                    ? 'fa-solid fa-pen-to-square me-2 theme'
+                    : 'fa-solid fa-floppy-disk theme'
+                }
+                onClick={() => {
+                  onEditableClick()
+                  setEditSelected(null)
+                  // if (isTableEditable) {
+                  //   onTableUpdate(editedTableObject)
+                  // }
+                }}
+              />
+              <i
+                role={'button'}
+                className="fa-solid fa-trash ms-2 me-0"
+                onClick={() => {
+                  onDeleteComponent()
+                }}
+              ></i>
             </div>
-          ) : null}
+          )}
+        </div>
+        <div className="row">
+          <div className="border w-100 p-0 product-detail-table">
+            <DataTable
+              sortIcon={<i className="fa-solid fa-sort ms-1"></i>}
+              pagination={false}
+              paginationPerPage={false}
+              fixedHeader
+              columns={tableHeader}
+              data={tableRows}
+              customStyles={customStyles}
+              persistTableHead
+              sortFunction={customSort}
+              // conditionalRowStyles={conditionalRowStyles}
+              // selectableRows
+              // onSelectedRowsChange={selectedRowsActionUA}
+            />
+            {isEditable ? (
+              renderDummyRow()
+            ) : isAdmin && !isTableEditable && !archivedFilter ? (
+              <div
+                role={'button'}
+                className="add-row d-none d-lg-flex"
+                onClick={() => {
+                  setIsEditable(true)
+                }}
+              >
+                <img
+                  src={Plusicon}
+                  style={{
+                    width: '16px',
+                    marginRight: '12px',
+                  }}
+                />
+                Add
+              </div>
+            ) : null}
+          </div>
         </div>
       </div>
-    </div>
+      <Modal
+        show={
+          (deleteSelected !== null && Object.keys(deleteSelected).length > 0) ||
+          archiveSelected !== null
+        }
+        centered
+        onHide={() => {
+          setDeleteSelected(null)
+          setArchiveSelected(null)
+        }}
+      >
+        <Modal.Header
+          style={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            borderBottom: '0',
+          }}
+        >
+          <Modal.Title>
+            {deleteSelected !== null && Object.keys(deleteSelected).length > 0
+              ? 'Delete Row'
+              : 'Archive Row'}
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body
+          style={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            textAlign: 'center',
+            borderBottom: '0',
+            fontWeight: '600',
+          }}
+        >
+          {deleteSelected !== null && Object.keys(deleteSelected).length > 0
+            ? 'Are you sure you want to delete this row?'
+            : 'Are you sure you want to archive this row?'}
+        </Modal.Body>
+        <Modal.Footer
+          style={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            borderTop: '0',
+          }}
+          centered
+        >
+          <button
+            id="mybtn"
+            className="btn btn-background me-4"
+            onClick={() => {
+              setDeleteSelected(null)
+              setArchiveSelected(null)
+            }}
+          >
+            Cancel
+          </button>
+          <button
+            className="btn"
+            onClick={() => {
+              if (deleteSelected !== null && Object.keys(deleteSelected).length > 0) {
+                deleteRow(deleteSelected)
+              }
+              if (archiveSelected !== null) {
+                archiveRow(archiveSelected)
+              }
+            }}
+          >
+            Confirm
+          </button>
+        </Modal.Footer>
+      </Modal>
+    </>
   )
 }
