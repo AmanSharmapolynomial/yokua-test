9aba14c289cf5b468c4ce033b4b460ef37897dc3
Ankur Khandelwal <khandelwalankur176@gmail.com>
2022-09-05 21:34:27 +0530

    fixed bugs in refresh token api

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,5 +1,13 @@
 import { create } from 'axios'
-import { getToken, removeToken, getRefreshToken, setToken, setRefreshToken } from './token'
+import {
+  getToken,
+  removeToken,
+  getRefreshToken,
+  setToken,
+  setRefreshToken,
+  removeUserEmail,
+  removeUserRole,
+} from './token'
 import { toast } from 'react-toastify'
 const API = create({ baseURL: 'https://yokogawa-flow-center.herokuapp.com/' })
 
@@ -21,57 +29,40 @@ API.interceptors.request.use(
 )
 
 API.interceptors.response.use(null, async e => {
-  if (e.response.status === 401 && refreshFlag === 0) {
-    const originalConfig = e.config
-    if (originalConfig.url !== '/auth/login' && e.response) {
-      // Access Token was expired
-      if (!originalConfig._retry) {
-        refreshFlag = 1
-        originalConfig._retry = true
-        try {
-          console.log('inside TRY')
-          const rs = await API.post('/auth/token/refresh/', {
-            refresh: getRefreshToken(),
-          })
-          refreshFlag = 0
-          const { access: accessToken, refresh: refreshToken } = rs.data
-          setToken(accessToken)
-          setRefreshToken(refreshToken)
-          //toast.success('Token refreshed successfully')
-          window.location.reload()
-          return instance(originalConfig)
-        } catch (err) {
-          if (originalConfig.url == '/auth/token/refresh/') {
-            toast.error('Session Expired')
-            removeToken()
-            window.location.href = '/auth/login'
-          }
-          return Promise.reject(err)
-        }
-      }
-    } else {
+  if (e.response.status === 401 && e.config.url !== '/auth/login' && refreshFlag === 0) {
+    try {
+      console.log('Inside TRY')
+      refreshFlag = 1
+      const rs = await API.post('/auth/token/refresh/', {
+        refresh: getRefreshToken(),
+      })
+      const { access, refresh } = rs.data
+      toast.success('Refreshing Website')
+      setToken(access)
+      setRefreshToken(refresh)
+      window.location.reload()
+    } catch (error) {
+      console.log('Inside CATCH')
       removeToken()
+      removeUserEmail()
+      removeUserRole()
       window.location.href = '/auth/login'
     }
-    //return Promise.reject(err)
   }
 
   if (Array.isArray(e.response.data?.message) && e.response.status !== 401) {
     toast.error(e.response.data?.message[0])
   } else if (e.response.data?.message && e.response.data?.message[0] && e.response.status !== 401) {
     toast.error(e.response.data?.message)
-  } else {
-    toast.success('Refreshing the Website')
   }
-  if (Array.isArray(e.response.data?.email)) toast.error(e.response.data?.email[0])
-  if (Array.isArray(e.response.data?.new_password2)) toast.error(e.response.data?.new_password2[0])
-  if (Array.isArray(e.response.data?.new_password2)) toast.error(e.response.data?.new_password2[1])
+  toast.error(e.response.data?.email[0])
+  toast.error(e.response.data?.new_password2[0])
+  toast.error(e.response.data?.new_password2[1])
 
   if (e.response.status == 400) {
     e.response.data.message?.forEach(message => {
       toast.error(message)
     })
-
     e.response.data.email?.forEach(email => {
       toast.error(email)
     })
