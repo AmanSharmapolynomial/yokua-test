d41dd084968c14011687a6369736126f41b13b97
musaib-js <musaibaltafurahman@gmail.com>
2023-06-27 18:17:11 +0530

    Fix: Images going out of box in product questions

--- src/screens/contact/Contact.js
+++ src/screens/contact/Contact.js
@@ -549,7 +549,7 @@ export default () => {
                             style={{
                               objectFit: 'cover',
                               width: '100%',
-                              height: '100%',
+                              // height: '100%',
                             }}
                           />
                         </div>
